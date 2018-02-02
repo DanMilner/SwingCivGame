3ced49b4fcbaa561afb5e3eb4d74aa2c37a9a8c9
Daniel Milner <DanielMMilner@gmail.com>
2018-02-02 16:22:27 +1100

    removed unused function

--- src/main/ResourceTypes.java
+++ src/main/ResourceTypes.java
@@ -1,9 +1,5 @@
 package main;
 
 public enum ResourceTypes {
-    WOOD, IRON, GOLD, COAL, COPPER, STONE, FOOD, WATER, DIAMONDS, GRASS, SAND, SNOW;
-
-    public static int getNumberOfResourceTypes() {
-        return ResourceTypes.values().length;
-    }
+    WOOD, IRON, GOLD, COAL, COPPER, STONE, FOOD, WATER, DIAMONDS, GRASS, SAND, SNOW
 }
